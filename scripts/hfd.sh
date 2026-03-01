7ad42017f7e98e07037f236aefd30a5205ac819d
Allen Guo <guosonglin001@gmail.com>
2026-03-02 00:20:07 +0800

    granite-docling-258M-GGUF

--- scripts/hfd.sh
+++ scripts/hfd.sh
@@ -24,6 +24,9 @@ hfd.sh mradermacher/LightOnOCR-2-1B-GGUF \
   --include "LightOnOCR-2-1B.f16.gguf" --include "LightOnOCR-2-1B.mmproj-f16.gguf"
 # https://huggingface.co/mradermacher/LightOnOCR-2-1B-GGUF/tree/main
 
+hfd.sh ggml-org/granite-docling-258M-GGUF
+# https://huggingface.co/ggml-org/granite-docling-258M-GGUF
+
 # -----
 
 hfd.sh mlx-community/GLM-OCR-bf16
