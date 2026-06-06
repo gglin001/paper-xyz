b86e27f1e4e5c0482b569e519310f512d94163d5
Allen Guo <guosonglin001@gmail.com>
2026-06-07 00:41:16 +0800

    misc

--- agent/llama-serve.sh
+++ agent/llama-serve.sh
@@ -5,9 +5,11 @@ set -euo pipefail
 
 args=(
   #
-  -m third_party/GLM-OCR-GGUF/GLM-OCR-Q8_0.gguf
-  -mm third_party/GLM-OCR-GGUF/mmproj-GLM-OCR-Q8_0.gguf
+  # -m third_party/GLM-OCR-GGUF/GLM-OCR-Q8_0.gguf
+  # -mm third_party/GLM-OCR-GGUF/mmproj-GLM-OCR-Q8_0.gguf
   #
+  -m third_party/FireRed-OCR-GGUF/FireRed-OCR.Q8_0.gguf
+  -mm third_party/FireRed-OCR-GGUF/FireRed-OCR.mmproj-Q8_0.gguf
   #
   # -m third_party/GLM-OCR-GGUF-mradermacher/GLM-OCR.f16.gguf
   # -mm third_party/GLM-OCR-GGUF-mradermacher/GLM-OCR.mmproj-f16.gguf
