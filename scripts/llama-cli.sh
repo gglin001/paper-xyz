e9070f23727263d6dcc67b3ccbd4843318f5c7fd
Allen Guo <guosonglin001@gmail.com>
2026-08-10 23:34:05 +0800

    misc

--- scripts/llama-cli.sh
+++ scripts/llama-cli.sh
@@ -5,17 +5,17 @@ set -euo pipefail
 
 args=(
   #
-  # -m third_party/Unlimited-OCR-GGUF/Unlimited-OCR-Q8_0.gguf
-  # -mm third_party/Unlimited-OCR-GGUF/mmproj-Unlimited-OCR-F16.gguf
-  # --prompt "Parse this document and convert it into standard markdown format."
+  -m third_party/Unlimited-OCR-GGUF/Unlimited-OCR-Q8_0.gguf
+  -mm third_party/Unlimited-OCR-GGUF/mmproj-Unlimited-OCR-F16.gguf
+  --prompt "document parsing."
   #
   # -m third_party/dots.mocr-gguf/dotsmocr-1.8b-q8_0.gguf
   # -mm third_party/dots.mocr-gguf/mmproj-dotsmocr-bf16.gguf
   # --prompt "Parse this document and convert it into standard markdown format."
   #
-  -m third_party/chandra-ocr-2-GGUF/chandra-ocr-2.Q8_0.gguf
-  -mm third_party/chandra-ocr-2-GGUF/chandra-ocr-2.mmproj-bf16.gguf
-  --prompt "document parsing."
+  # -m third_party/chandra-ocr-2-GGUF/chandra-ocr-2.Q8_0.gguf
+  # -mm third_party/chandra-ocr-2-GGUF/chandra-ocr-2.mmproj-bf16.gguf
+  # --prompt "Parse this document and convert it into standard markdown format."
   #
   # -n 10000
   # --temp 0.0
