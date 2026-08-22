12e749d0770603a2a235db67f200e986b459cca0
Allen Guo <guosonglin001@gmail.com>
2026-08-22 19:36:59 +0800

    mlx

--- scripts/mlx_vlm.generate.sh
+++ scripts/mlx_vlm.generate.sh
@@ -5,12 +5,15 @@ set -euo pipefail
 
 args=(
   #
-  --model third_party/dots.mocr-8bit
+  # --model third_party/dots.mocr-8bit
+  # --prompt "Parse this document and convert it into standard markdown format."
+  #
+  --model third_party/OvisOCR2-8bit
+  --prompt "Extract all readable content from the image in natural human reading order and output the result as a single Markdown document. For charts or images, represent them using an HTML image tag: <' + 'img src="images/bbox_{left}_{top}_{right}_{bottom}.jpg" />, where left, top, right, bottom are bounding box coordinates scaled to [0, 1000). Format formulas as LaTeX. Format tables as HTML: <table>...</table>. Transcribe all other text as standard Markdown. Preserve the original text without translation or paraphrasing."
   #
   --max-tokens 10000
   --temperature 0.0
   #
-  --prompt "Parse this document and convert it into standard markdown format."
   #
   --image agent/demo.png
   #
